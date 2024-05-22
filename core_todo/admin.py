06456a3ac43d2412d17caa8465eafce95ade0fd3
Alona <alona.sorochynska@gmail.com>
2024-05-22 10:36:44 +0100

    register admins in admin.py, create view for index.html, create templates dir and add index.html

--- core_todo/admin.py
+++ core_todo/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Tag
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ("username", "first_name", "last_name",)
+
+
+admin.site.register(Tag)
+admin.site.unregister(Group)
--- core_todo/views.py
+++ core_todo/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from core_todo.models import Task, Tag
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_tasks = Task.objects.count()
+    num_tags = Tag.objects.count()
+
+    context = {
+        "num_tasks": num_tasks,
+        "num_tags": num_tags,
+    }
+
+    return render(request, "core_todo/index.html", context=context)
