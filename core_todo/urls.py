4dc8602497b34557490baffc1ff2efb5a0681311
Alona <alona.sorochynska@gmail.com>
2024-05-22 11:06:47 +0100

    fill sidebar.html, add TaskListView, TagListView, create urls for this views

--- core_todo/urls.py
+++ core_todo/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from core_todo.views import index
-
+from core_todo.views import index, TaskListView, TagListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
     ]
 
 app_name = "core_todo"
--- core_todo/views.py
+++ core_todo/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
 from core_todo.models import Task, Tag
 
@@ -15,3 +18,19 @@ def index(request):
     }
 
     return render(request, "core_todo/index.html", context=context)
+
+
+class TaskListView(LoginRequiredMixin, generic.ListView):
+    model = Task
+    queryset = Task.objects.all().prefetch_related("tags")
+    # paginate_by = 5
+
+
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Task
+    # form_class = TaskCreateForm
+    success_url = reverse_lazy("core_todo:task-list")
+
+
+class TagListView(LoginRequiredMixin, generic.ListView):
+    model = Task
