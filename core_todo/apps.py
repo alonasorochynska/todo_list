ed0018afd8f241080754368549c7e8ddf749f1f3
Alona <alona.sorochynska@gmail.com>
2024-05-22 09:23:52 +0100

    replace all singe quotes to double quotes in todo_list and core_todo

--- core_todo/apps.py
+++ core_todo/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class CoreTodoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core_todo'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "core_todo"
